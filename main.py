72fcaa7597cb596802cb72ecb4b4459cdf929b5f
LX7 <linoilday@gmail.com>
2023-06-06 18:31:34 +0200

    added function to be able to see where you moved the chess piece

--- main.py
+++ main.py
@@ -17,81 +17,92 @@ board_surface = pygame.transform.scale(board_surface, board_size)
 
 # classes
 class ChessPiece(pygame.sprite.Sprite):
-    def __init__(self, pos, color, image_path):
-        super().__init__()
-        self.color = color
-        self.image_orig = pygame.image.load(image_path).convert_alpha()
-        self.image = pygame.transform.scale(self.image_orig, (int(self.image_orig.get_width() * 0.5), int(self.image_orig.get_height() * 0.5)))
-        self.rect = self.image.get_rect()
-        self.rect.center = pos
-        self.mouse_click = False
-        self.piece_selected = False
+  def __init__(self, pos, color, image_path):
+    super().__init__()
+    self.color = color
+    self.image_orig = pygame.image.load(image_path).convert_alpha()
+    self.image = pygame.transform.scale(self.image_orig, (int(self.image_orig.get_width() * 0.5), int(self.image_orig.get_height() * 0.5)))
+    self.rect = self.image.get_rect()
+    self.rect.center = pos
+    
+    self.mouse_click = False
+    self.piece_selected = False
+    self.piece_previous_pos = self.rect.center
+    self.piece_previous_pos_xy = (self.rect.left, self.rect.top)
 
-    def move(self):
-        self.mouse_pos = pygame.mouse.get_pos()
-        self.mouse_buttons = pygame.mouse.get_pressed()
-        self.piece_pos = self.rect.center
-        
-        if self.rect.collidepoint(self.mouse_pos) and self.mouse_buttons[0] and not self.mouse_click:
-            self.mouse_click = True
-            self.piece_selected = True
-        
-        if self.mouse_buttons[0] and self.piece_selected and not self.mouse_click:
-            self.y = ((self.mouse_pos[0] // 100) * 100) + 50
-            self.x = ((self.mouse_pos[1] // 100) * 100) + 50
-            self.rect.center = (self.y, self.x)
-            self.piece_selected = False
-            self.mouse_click = True
-
-        if not self.mouse_buttons[0]:
-            self.mouse_click = False
+ 
+  def move(self):
+    self.mouse_pos = pygame.mouse.get_pos()
+    self.mouse_buttons = pygame.mouse.get_pressed()
+    self.piece_pos = self.rect.center # for the take_piece function
     
-    def highlight_piece(self):
-        if self.piece_selected:
-            pygame.draw.rect(screen, "Green", self.rect, 3)
+    if self.rect.collidepoint(self.mouse_pos) and self.mouse_buttons[0] and not self.mouse_click:
+      self.mouse_click = True
+      self.piece_selected = True
     
-    def take_piece(self): 
-        for sprite in pieces.sprites():
-            if sprite != self and self.rect.colliderect(sprite.rect) and not self.piece_selected:
-                if sprite.color != self.color:
-                    sprite.kill()
-                else:
-                    self.rect.center = self.piece_pos
+    if self.mouse_buttons[0] and self.piece_selected and not self.mouse_click:
+      self.y = ((self.mouse_pos[0] // 100) * 100) + 50
+      self.x = ((self.mouse_pos[1] // 100) * 100) + 50
+      self.rect.center = (self.y, self.x)
+      self.piece_selected = False
+      self.mouse_click = True
     
-    def update(self):
-        self.move()
-        self.highlight_piece()
-        self.take_piece()
+    if not self.mouse_buttons[0]:
+      self.mouse_click = False
+    
+  
+  def highlight_piece(self):
+    if self.piece_selected:
+      pygame.draw.rect(screen, (255, 0, 0), self.rect, 3)
+      self.piece_previous_pos = self.rect.center
+      self.piece_previous_pos_xy = (self.rect.left, self.rect.top)
+    elif self.rect.center != self.piece_previous_pos:
+      pygame.draw.rect(screen, (0, 255, 0), self.rect, 3)
+      pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(self.piece_previous_pos_xy, self.rect.size), 3)
+  
+  def take_piece(self): 
+    for sprite in pieces.sprites():
+      if sprite != self and self.rect.colliderect(sprite.rect) and not self.piece_selected:
+        if sprite.color != self.color:
+          sprite.kill()
+        else:
+          self.rect.center = self.piece_pos
+    
+  
+  def update(self):
+    self.move()
+    self.highlight_piece()
+    self.take_piece()
 
 class pawn(ChessPiece):
-    def __init__(self, pos, color):
-        image_path = "images/pieces/blackPawn.png" if color == "black" else "images/pieces/whitePawn.png"
-        super().__init__(pos, color, image_path)
+  def __init__(self, pos, color):
+    image_path = "images/pieces/blackPawn.png" if color == "black" else "images/pieces/whitePawn.png"
+    super().__init__(pos, color, image_path)
 
 class knight(ChessPiece):
-    def __init__(self, pos, color):
-        image_path = "images/pieces/blackKnight.png" if color == "black" else "images/pieces/whiteKnight.png"
-        super().__init__(pos, color, image_path)
+  def __init__(self, pos, color):
+    image_path = "images/pieces/blackKnight.png" if color == "black" else "images/pieces/whiteKnight.png"
+    super().__init__(pos, color, image_path)
 
 class bishop(ChessPiece):
-    def __init__(self, pos, color):
-        image_path = "images/pieces/blackBishop.png" if color == "black" else "images/pieces/whiteBishop.png"
-        super().__init__(pos, color, image_path)
+  def __init__(self, pos, color):
+    image_path = "images/pieces/blackBishop.png" if color == "black" else "images/pieces/whiteBishop.png"
+    super().__init__(pos, color, image_path)
 
 class rook(ChessPiece):
-    def __init__(self, pos, color):
-        image_path = "images/pieces/blackRook.png" if color == "black" else "images/pieces/whiteRook.png"
-        super().__init__(pos, color, image_path)
+  def __init__(self, pos, color):
+    image_path = "images/pieces/blackRook.png" if color == "black" else "images/pieces/whiteRook.png"
+    super().__init__(pos, color, image_path)
 
 class queen(ChessPiece):
-    def __init__(self, pos, color):
-        image_path = "images/pieces/blackQueen.png" if color == "black" else "images/pieces/whiteQueen.png"
-        super().__init__(pos, color, image_path)
+  def __init__(self, pos, color):
+    image_path = "images/pieces/blackQueen.png" if color == "black" else "images/pieces/whiteQueen.png"
+    super().__init__(pos, color, image_path)
 
 class king(ChessPiece):
-    def __init__(self, pos, color):
-        image_path = "images/pieces/blackKing.png" if color == "black" else "images/pieces/whiteKing.png"
-        super().__init__(pos, color, image_path)
+  def __init__(self, pos, color):
+    image_path = "images/pieces/blackKing.png" if color == "black" else "images/pieces/whiteKing.png"
+    super().__init__(pos, color, image_path)
 
 # Group
 pieces = pygame.sprite.Group()
